c91cab2331489ef1a7cf4fd1f5944186b1601d18
Asami Doi <doiasami1219@gmail.com>
2019-02-17 16:38:53 -0800

    Add command line arguments and buttons

--- generator.cpp
+++ generator.cpp
@@ -32,6 +32,8 @@ static double kparam[][2] = {{0, 0}, {0, 0}};
 static int timestep = 0;
 static Point points[NPOINTS];
 static std::vector<std::vector<Point>> result;
+static int seed = 1;
+static int maxgen = 100000;
 
 // Graph
 static std::vector<XV> xv;
@@ -49,6 +51,7 @@ static Point computeCenter() {
 
 static void initPoints() {
   std::default_random_engine gen;
+  gen.seed(seed);
   std::normal_distribution<double> dist(0, 1);
 
   for (int i = 0; i < NPOINTS; i++) {
@@ -133,6 +136,10 @@ static void step() {
 static void printHeader() {
   std::cout << R"END(<html>
 <body>
+<canvas id=canvas width=650 height=650></canvas>
+timestep: <span id=timestep></span>
+<button id=start>Stop</button>
+<button id=reset>Reset</button>
 <script>
 )END";
 }
@@ -141,6 +148,121 @@ static void printFooter() {
   std::cout << R"END(</script>
 </body>
 </html>
+)END";
+}
+
+static void printScript() {
+  std::cout << R"END(
+// Main canvas
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+ctx.scale(0.65, 0.65);
+ctx.translate(500, 500);
+
+// HTML elements
+const startButton = document.getElementById('start');
+const resetButton = document.getElementById('reset');
+
+// Constants
+const RED = 0;
+const BLUE = 1;
+
+// Variables
+let currentScale = 1;
+let handle;
+let index = 0;
+
+function drawGrid() {
+  ctx.save();
+  ctx.strokeStyle = '#aaa';
+  ctx.lineWidth = 1 / currentScale;
+
+  const len = 5000;
+  for (let x = -len; x < len; x += 10) {
+    ctx.beginPath();
+    ctx.moveTo(x, -len);
+    ctx.lineTo(x, len);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(-len, x);
+    ctx.lineTo(len, x);
+    ctx.stroke();
+  }
+  ctx.restore();
+}
+
+function drawPoint(p) {
+  ctx.save();
+  ctx.fillStyle = (p.color == RED) ? 'red' : 'blue';
+  ctx.beginPath();
+  ctx.arc(p.x, p.y, 5 / currentScale, 0, 2 * Math.PI, true);
+  ctx.fill();
+  ctx.restore();
+}
+
+function scaleout() {
+  let max = 0;
+  for (let i = 1; i < points[index].length; i++) {
+    const p = points[index][i];
+    max = Math.max(max, Math.abs(p.x), Math.abs(p.y));
+  }
+
+  currentScale = 500 / (max * 1.2);
+  ctx.scale(currentScale, currentScale);
+}
+
+function redraw() {
+  ctx.save();
+  ctx.clearRect(-50000, -50000, 100000, 100000);
+  scaleout();
+  drawGrid();
+  for (let i = 1; i < points[index].length; i++) {
+    drawPoint(points[index][i]);
+  }
+  document.getElementById('timestep').innerText = points[index][0];
+  ctx.restore();
+}
+
+function step() {
+  if (index < points.length) {
+    redraw();
+    index++;
+  } else {
+    stop();
+  }
+}
+
+function start() {
+  handle = window.setInterval(step, 0);
+  startButton.innerText = 'Stop';
+}
+
+function stop() {
+  window.clearInterval(handle);
+  handle = undefined;
+  startButton.innerText = 'Start';
+}
+
+function reset() {
+  const running = handle;
+  if (running) stop();
+  index = 0;
+  redraw();
+  if (running) start();
+}
+
+startButton.addEventListener('click', function() {
+  if (handle) {
+    stop();
+  } else {
+    start();
+  }
+});
+
+resetButton.addEventListener('click', reset);
+
+start();
 )END";
 }
 
@@ -162,20 +284,65 @@ static void printXV() {
   std::cout << "const xv = [\n";
   for (int i = 0; i < xv.size(); i += 100) {
     std::cout << "  [" << i << ", "
-              << xv[i].x << ", "
-              << xv[i].v << "],\n";
+              << "{x:" << xv[i].x 
+              << ",y:" <<xv[i].v
+              << "}],\n";
   }
   std::cout << "];\n";
 }
 
-int main() {
+static void usage() {
+  std::cerr << "Usage: generator [ -k k0 k1 k2 k3 ] [ -seed number ] [ -gen number ]\n";
+  exit(1);
+}
+
+static void parseArgs(int argc, char **argv) {
+  while (argc > 0) {
+    if (strcmp("-k", argv[0]) == 0) {
+      if (argc < 5)
+        usage();
+      kparam[0][0] = std::stod(argv[1]);
+      kparam[0][1] = std::stod(argv[2]);
+      kparam[1][0] = std::stod(argv[3]);
+      kparam[1][1] = std::stod(argv[4]);
+      argc -= 5;
+      argv += 5;
+      continue;
+    }
+
+    if (strcmp("-seed", argv[0]) == 0) {
+      if (argc < 2)
+        usage();
+      seed = std::stoi(argv[1]);
+      argc -= 2;
+      argv += 2;
+      continue;
+    }
+
+    if (strcmp("-gen", argv[0]) == 0) {
+      if (argc < 2)
+        usage();
+      maxgen = std::stoi(argv[1]);
+      argc -= 2;
+      argv += 2;
+      continue;
+    }
+
+    usage();
+  }
+}
+
+int main(int argc, char **argv) {
+  parseArgs(argc - 1, argv + 1);
+
   initPoints();
-  for (int i = 0; i < 100000; i++)
+  for (int i = 0; i < maxgen; i++)
     step();
 
   printHeader();
   printPoints();
   printXV();
+  printScript();
   printFooter();
 }
 
