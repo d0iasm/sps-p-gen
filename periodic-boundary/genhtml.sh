a8b495c2992e891bf428f0a257e01c0d54b379cc
Asami Doi <doiasami1219@gmail.com>
2019-04-18 22:20:48 +0900

    Define functions to make it easier to read code.

--- periodic-boundary/genhtml.sh
+++ periodic-boundary/genhtml.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 
-out="../../sps-p-out/periodic-boundary/"
-index="index.html"
-maxgen=100000
+# Constant definition.
+OUT="../../sps-p-out/periodic-boundary/"
+INDEX="index.html"
+MAX_GEN=100000
 
-make generator
+# Function definition.
+check_dependencies() {
+  if ! [ -x "$(command -v clang++)" ]; then
+    echo 'Error: clang++ is not installed.' >&2
+    exit 1
+  fi
+  if ! [ -x "$(command -v parallel)" ]; then
+    echo 'Error: parallel is not installed.' >&2
+    exit 1
+  fi
+}
 
-for i in {-8..12}
-do
-  for j in {-8..12}
-  do
-    p=`echo $i | awk '{ printf "%.1f", $1 / 10 }'`
-    m=`echo $j | awk '{ printf "%.1f", $1 / 10 }'`
-    file=$out"abpm_0.8,0.4,"$p","$m".html"
-    ./generator -k2 0.8 0.4 $p $m -gen $maxgen > $file 
-    echo $file
+exec_generator() {
+  for i in {-8..12}; do
+    for j in {-8..12}; do
+      local p=`echo $i | awk '{ printf "%.1f", $1 / 10 }'`
+      local m=`echo $j | awk '{ printf "%.1f", $1 / 10 }'`
+      local file=$OUT"abpm_0.8,0.4,"$p","$m".html"
+      ./generator -k2 0.8 0.4 $p $m -gen $MAX_GEN > $file 
+      echo Generated $file
+    done
   done
-done
+}
 
-cd $out
-echo "<h1>SPS-P Model Simulation</h1><ul>" > $index
-for i in *.html; do echo "<li><a href="$i">$i</a></li>"; done >> $index
-echo "</ul>" >> $index
-echo Done Write to $index
+make_index() {
+  cd $out
+  echo "<h1>SPS-P Model Simulation</h1><ul>" > $INDEX
+  for i in *.html; do echo "<li><a href="$i">$i</a></li>"; done >> $INDEX
+  echo "</ul>" >> $INDEX
+  echo Generated $INDEX
+}
+
+# Execution.
+check_dependencies 
+make generator
+exec_generator
+make_index
+
