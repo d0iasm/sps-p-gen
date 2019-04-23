0ecf89d3d6fba2474678e03072eeaee47269e720
Asami Doi <doiasami1219@gmail.com>
2019-04-23 15:59:10 +0900

    Copy generator shell script from periodic to open.

--- open-boundary/genhtml.sh
+++ open-boundary/genhtml.sh
@@ -1,25 +1,79 @@
 #!/bin/bash
+# This shell script is using GNU Parallel.
+# Because of an academic tradition, Cite:
+# @book{tange_ole_2018_1146014,
+#   author       = {Tange, Ole},
+#   title        = {GNU Parallel 2018},
+#   publisher    = {Ole Tange},
+#   month        = Mar,
+#   year         = 2018,
+#   ISBN         = {9781387509881},
+#   doi          = {10.5281/zenodo.1146014},
+#   url          = {https://doi.org/10.5281/zenodo.1146014}
+# }
 
-out="../sps-p-out/"
-index="out/index.html"
-maxgen=100000
+# More about funding GNU Parallel and the citation notice:
+# https://lists.gnu.org/archive/html/parallel/2013-11/msg00006.html
+# https://www.gnu.org/software/parallel/parallel_design.html#Citation-notice
+# https://git.savannah.gnu.org/cgit/parallel.git/tree/doc/citation-notice-faq.txt
 
-clang++ -ffast-math -O3 -march=native -std=c++11 -o generator generator.cpp
+# If you send a copy of your published article to tange@gnu.org, it will be
+# mentioned in the release notes of next version of GNU Parallel.
 
-for i in {-8..12}
-do
-  for j in {-8..12}
-  do
-    p=`echo $i | awk '{ printf "%.1f", $1 / 10 }'`
-    m=`echo $j | awk '{ printf "%.1f", $1 / 10 }'`
-    file=$out"abpm_0.8,0.4,"$p","$m".html"
-    ./generator -k2 0.8 0.4 $p $m -gen $maxgen > $file 
-    echo $file
-  done
-done
 
-cd out/
-echo "<h1>SPS-P Model Simulation</h1><ul>" > index.html 
-for i in *.html; do echo "<li><a href="$i">$i</a></li>"; done >> index.html
-echo "</ul>" >> index.html 
-echo Done Write to index.html 
+# Constant definition.
+SRC=$(pwd)
+DEST="../../sps-p-out/open-boundary/"
+INDEX="index.html"
+MAX_GEN=100000
+
+# Function definition.
+check_dependencies() {
+  if ! [ -x "$(command -v clang++)" ]; then
+    echo 'Error: clang++ is not installed.' >&2
+    exit 1
+  fi
+  if ! [ -x "$(command -v parallel)" ]; then
+    echo 'Error: parallel is not installed.' >&2
+    exit 1
+  fi
+}
+
+exec_generator() {
+  local range="-0.8 -0.7 -0.6 -0.5 -0.4 -0.3 -0.2 -0.1 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2"
+  parallel ./generator -k2 0.8 0.4 '{1}' '{2}' -gen $MAX_GEN '>' $DEST'abpm=0.8,0.4,{1},{2}.html' ::: $range ::: $range
+}
+
+make_index() {
+  cd $DEST
+  echo "<h1>SPS-P Model Simulation: Open Boundary</h1><ul>" > $INDEX
+  for i in *.html; do echo "<li><a href="$i">$i</a></li>"; done >> $INDEX
+  echo "</ul>" >> $INDEX
+  echo Generated $INDEX
+  cd $SRC
+}
+
+copy_js() {
+  local dest=$DEST/script.js
+  if [ -f $dest ]; then
+    rm $dest 
+  fi
+  cp ./script.js $dest 
+}
+
+copy_css() {
+  local dest=$DEST/style.css
+  if [ -f $dest ]; then
+    rm $dest 
+  fi
+  cp ./style.css $dest 
+}
+
+# Execution.
+check_dependencies 
+make generator
+exec_generator
+make_index
+copy_js
+copy_css
+
