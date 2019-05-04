2410aed0cf532c2938e1074dc2e6e015f393f463
Asami Doi <doiasami1219@gmail.com>
2019-05-04 20:13:53 +0900

    Update gencsv shell to use parallel command.

--- open-boundary/gencsv.sh
+++ open-boundary/gencsv.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
+# This shell script is using GNU Parallel.
+# Because of an academic tradition, Cite:
+# @book{tange_ole_2018_1146014,
+#   author       = {Tange, Ole},
+#   title        = {GNU Parallel 2018},
+#   publisher    = {Ole Tange},
+#   month        = Mar,
+#   year         = 2018,
+#   ISBN         = {9781387509881},
+#   doi          = {10.5281/zenodo.1146014},
+#   url          = {https://doi.org/10.5281/zenodo.1146014}
+# }
 
-MAX_GEN=100
+# More about funding GNU Parallel and the citation notice:
+# https://lists.gnu.org/archive/html/parallel/2013-11/msg00006.html
+# https://www.gnu.org/software/parallel/parallel_design.html#Citation-notice
+# https://git.savannah.gnu.org/cgit/parallel.git/tree/doc/citation-notice-faq.txt
+
+# If you send a copy of your published article to tange@gnu.org, it will be
+# mentioned in the release notes of next version of GNU Parallel.
+
+# Constant definition.
+MAX_GEN=10000
 OUT="csv/result.csv"
 
-# Execution.
-make generator
-echo "k1,k00,k01,k10,k11,k2,ka,kb,kp,km,class,evergy(average),energy(variance)" > $OUT
+# Function definition.
+check_dependencies() {
+  if ! [ -x "$(command -v clang++)" ]; then
+    echo 'Error: clang++ is not installed.' >&2
+    exit 1
+  fi
+  if ! [ -x "$(command -v parallel)" ]; then
+    echo 'Error: parallel is not installed.' >&2
+    exit 1
+  fi
+}
 
-for i in {-8..12}
-do
-  for j in {-8..12}
-  do
-    p=`echo $i | awk '{ printf "%.1f", $1 / 10 }'`
-    m=`echo $j | awk '{ printf "%.1f", $1 / 10 }'`
-    ./generator -k2 0.8 0.4 $p $m -gen $MAX_GEN -csv >> $OUT 
-    echo "done 0.8 0.4 "$p $m 
-  done
-done
+exec_generator_csv() {
+  echo "k1,k00,k01,k10,k11,k2,ka,kb,kp,km,class,evergy(average),energy(variance)" > $OUT
+  local range="-0.8 -0.7 -0.6 -0.5 -0.4 -0.3 -0.2 -0.1 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2"
+  parallel ./generator -k2 0.8 0.4 '{1}' '{2}' -gen $MAX_GEN -csv '>' $OUT ::: $range ::: $range
+}
+
+# Execution.
+check_dependencies
+make generator
+exec_generator_csv
+
