f4d26143ec46b16dac57478cf956ba778412d811
Asami Doi <doiasami1219@gmail.com>
2019-05-04 09:52:45 +0900

    Rename variables.

--- open-boundary/gencsv.sh
+++ open-boundary/gencsv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-maxgen=100
+MAX_GEN=100
+OUT="csv/result.csv"
 
-clang++ -ffast-math -O3 -march=native -std=c++11 -o generator generator.cpp
-
-file="csv/result.csv"
-echo "k1,k00,k01,k10,k11,k2,ka,kb,kp,km,HBEaverage,HBEvariancen" > $file
+# Execution.
+make generator
+echo "k1,k00,k01,k10,k11,k2,ka,kb,kp,km,HBEaverage,HBEvariancen" > $OUT
 
 for i in {-8..12}
 do
@@ -13,7 +13,7 @@ do
   do
     p=`echo $i | awk '{ printf "%.1f", $1 / 10 }'`
     m=`echo $j | awk '{ printf "%.1f", $1 / 10 }'`
-    ./generator -k2 0.8 0.4 $p $m -gen $maxgen -csv >> $file 
+    ./generator -k2 0.8 0.4 $p $m -gen $MAX_GEN -csv >> $OUT 
     echo "done 0.8 0.4 "$p $m 
   done
 done
