ca7a06018ea9a95f4b63ba34423757afbda6a9db
Asami Doi <doiasami1219@gmail.com>
2019-05-17 21:53:30 +0900

    Add shell script for all generations.

---
+++ gen.sh
@@ -0,0 +1,13 @@
+# genhtml.sh
+./genhtml.sh -open
+./genhtml.sh -periodic
+./genhtml.sh -open -dist
+./genhtml.sh -periodic -dist
+
+# gencsv.sh
+./gencsv.sh -open
+./gencsv.sh -periodic
+
+# gencsve.sh
+./gencsve.sh -open
+./gencsve.sh -periodic
