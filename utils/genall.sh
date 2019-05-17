a25b222c11f66a60756bb319aba129fb9f943666
Asami Doi <doiasami1219@gmail.com>
2019-05-17 21:46:59 +0900

    Update shell script for utils.

--- utils/genall.sh
+++ utils/genall.sh
@@ -2,9 +2,9 @@
 
 
 # Class.
-python heatmap.py -z class -title "Class based on X and V"
+python3 heatmap.py -z class -title "Class based on X and V"
 
-python heatmap.py -z energy-average -title "Average of Macro Energy (open)" -src ../csv/open-boundary.csv
-python heatmap.py -z energy-variance -title "Variance of Macro Energy (open)" -src ../csv/open-boundary.csv
-python heatmap.py -z energy-average -title "Average of Macro Energy (periodic)" -src ../csv/periodic-boundary.csv
-python heatmap.py -z energy-variance -title "Variance of Macro Energy (periodic)" -src ../csv/periodic-boundary.csv
+# Macroscopic energy is same for open boundary and periodic boundary.
+python3 heatmap.py -z energy-average -title "Average of Macro Energy" -src ../csv/open-boundary.csv
+python3 heatmap.py -z energy-variance -title "Variance of Macro Energy" -src ../csv/open-boundary.csv
+
