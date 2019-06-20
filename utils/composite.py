b02f275ba42972290eba5e4fea5313419aad8548
Asami Doi <doiasami1219@gmail.com>
2019-06-20 18:30:03 +0900

    Rename titles of energy.

--- utils/composite.py
+++ utils/composite.py
@@ -2,10 +2,10 @@ from PIL import Image
 
 
 l1 = Image.open("../images/class-by-hand.png")
-l2 = Image.open("../images/macro-energy-average.png")
+l2 = Image.open("../images/Average of Macro Energy.png")
 r1 = Image.blend(l1, l2, alpha=.7)
 r1.save("../images/class-with-macro-energy-average.png")
 
-l3 = Image.open("../images/macro-energy-variance.png")
+l3 = Image.open("../images/Variance of Macro Energy.png")
 r2 = Image.blend(l1, l3, alpha=.7)
 r2.save("../images/class-with-macro-energy-variance.png")
--- utils/genall.sh
+++ utils/genall.sh
@@ -5,6 +5,6 @@
 python3 heatmap.py -z class -title "Class based on X and V"
 
 # Macroscopic energy is same for open boundary and periodic boundary.
-python3 heatmap.py -z energy-average -title "Average of Macro Energy" -src ../csv/open-boundary.csv
-python3 heatmap.py -z energy-variance -title "Variance of Macro Energy" -src ../csv/open-boundary.csv
+python3 heatmap.py -z energy-average -title "Average of Global Energy" -src ../csv/open-boundary.csv
+python3 heatmap.py -z energy-variance -title "Variance of Global Energy" -src ../csv/open-boundary.csv
 
